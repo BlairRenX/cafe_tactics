14833eb6672542a3a9c4bfd208c0b087d54aac6f
BlairRenX <blairrennex@gmail.com>
2021-08-27 14:04:08 +0100

    test commit
    some classes as well

---
+++ Blairmelia/Blairmelia/Assets/Scripts/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        private boolean hasUnit = false;
+        private int movementCost = -1; //that is to say, the Cell is inaccessible.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Blairmelia/Blairmelia/Assets/Scripts/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+//this class should probably be abstract.
+{
+    private String unitName = "Placeholder";
+    private Integer moveLength = 0;
+    private abstract Enum unitType { One, Two, Three } //placeholder type names. this should probably be put somewhere else. abstracted 'cuz it shouldn't be implemented ever.
+    private unitType thisUnitType = unitType.One
+    private List<Item> inventory = new List<Item>(); //this declaration smells like a problem-causing line of code. should be moved.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
